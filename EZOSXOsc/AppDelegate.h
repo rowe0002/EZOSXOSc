1039f0d28f36a1599a69e88d54cb002623db1180
rowe0002 <robert.rowe@nyu.edu>
2016-02-03 13:22:07 -0500

    Initial Commit

---
+++ EZOSXOsc/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EZOSXOsc
+//
+//  Created by Robert Rowe on 2/3/16.
+//  Copyright (c) 2016 Robert Rowe. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ EZOSXOsc/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  EZOSXOsc
+//
+//  Created by Robert Rowe on 2/3/16.
+//  Copyright (c) 2016 Robert Rowe. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
